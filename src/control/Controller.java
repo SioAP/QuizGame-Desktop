95f96c5b94a0d8a874fbc04097ede083914bcb34
kaledguz <guzman.kaled.pro@gmail.com>
2023-04-07 17:07:12 +0200

    fix vieww

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,90 +1,78 @@
 package control;
 
+import view.*;
+import websocket.*;
+import model.*;
+
+import com.esotericsoftware.kryonet.Connection;
+
 import data.DAOMySQL;
 import model.QuizGame;
 import model.User;
 import view.Router;
-import view.Router;
 import websocket.ClientWebsocket;
 
+import data.*;
+
 public class Controller {
-	
-	//specifications
-	//private Console myConsole;
+
+    //specifications
+    //private Console myConsole;
     private QuizGame myGame;
     private DAOMySQL myDAO;
     private ClientWebsocket myClient;
-    private User myUser;
     private Router myView;
-	
-	
-	//implementation
-	//constructor
-	public Controller() throws Exception {
-		/*this.laPartie = DAOStub.getRandomQuizSetForQuizGame();
-		this.laConsole = new Console(this);*/
-		
-		//data interactions
-		this.myDAO = new DAOMySQL(this);
-	//	this.myDAO.connectDatabase();
-		
-	//	this.myClient = new ClientWebsocket(this);
-	//	this.myClient.startClient();
-		
-	//	this.myGame = new QuizGame(this);
-		
-	this.myView = new Router(this);
-	this.myView.setVisible(true);
-	}
-	
-//	public void selectOption(Message packet, Connection c) throws Exception {
-//		if(packet.getNbSocket() == 1) {	this.checkRegister(packet, c); }
-//	}
+    private User myUser;
 
-//	private void checkRegister(Message packet, Connection c) {
-//		myUser = packet.getUser();
-//		if(packet.getBool()) {
-//			myUser.setConnected(true);
-//		} else {
-//			myUser.setConnected(false);
-//		}
-//	}
 
-	public ClientWebsocket getMyClient() {
-		return myClient;
-	}
+    //implementation
+    //constructor
+    public Controller() throws Exception {
+        //this.laPartie = DAOStub.getRandomQuizSetForQuizGame();
 
-	public QuizGame getMyGame() {
-		return myGame;
-	}
+        //data interactions
+        this.myDAO = new DAOMySQL(this);
+        this.myDAO.connectDatabase();
 
-	public void setMyGame(QuizGame theGame) {
-		this.myGame = theGame;
-	}
+        this.myUser = new User(this);
 
-	public DAOMySQL getMyDAO() {
-		return myDAO;
-	}
-	public User getMyUser() {
-		return myUser;
-	}
+        this.myClient = new ClientWebsocket(this);
 
-	public Router getMyView() {
-		return myView;
-	}
+        this.myView = new Router(this);
 
-	public void setMyView(Router myView) {
-		this.myView = myView;
-	}
+        this.myGame = new QuizGame(this);
 
-	public void setMyClient(ClientWebsocket myClient) {
-		this.myClient = myClient;
-	}
+    }
 
-	public void setMyUser(User myUser) {
-		this.myUser = myUser;
-	}
-	
-	
+    public void selectOption(Message packet, Connection c) throws Exception {
+        if(packet.getNbSocket() == 1) {     }
+    }
+
+    public ClientWebsocket getMyClient() {
+        return myClient;
+    }
+
+    public QuizGame getMyGame() {
+        return myGame;
+    }
+
+    public void setMyGame(QuizGame theGame) {
+        this.myGame = theGame;
+    }
+
+    public DAOMySQL getMyDAO() {
+        return myDAO;
+    }
+
+    public Router getMyView() {
+        return myView;
+    }
+
+    public void setMyView(Router myView) {
+        this.myView = myView;
+    }
+
+    public User getMyUser() {
+        return myUser;
+    }
 }
-
--- src/view/PnlLogin.java
+++ src/view/PnlLogin.java
@@ -8,78 +8,112 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import control.Controller;
-import model.User;
-
+import websocket.ClientWebsocket;
 
 public class PnlLogin extends JPanel {
-	
-	private Controller myController;
-	private JTextField txtLogin;
-	private JPasswordField txtPassword;
 
-	
+	private JPanel contentPane;
+	private JTextField txtLogin;
+	private JTextField txtPassword;
+	private Controller myController;
+	private ClientWebsocket myClient;
+	private JLabel lblMessage;
+
+	/**
+	 * Launch the application.
+	 */
 	public PnlLogin(Controller controller) {
 		myController = controller;
-	
+		myClient = myController.getMyClient();
 		
+	
 		setBounds(10, 10, 358, 294);
 		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 10, 358, 294);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		JLabel lblConnexion = new JLabel("Connexion");
 		lblConnexion.setFont(new Font("Tahoma", Font.PLAIN, 22));
 		lblConnexion.setHorizontalAlignment(SwingConstants.CENTER);
 		lblConnexion.setBounds(0, 0, 358, 59);
-		add(lblConnexion);
+		panel.add(lblConnexion);
 		
 		txtLogin = new JTextField();
 		txtLogin.setToolTipText("");
 		txtLogin.setHorizontalAlignment(SwingConstants.CENTER);
 		txtLogin.setBounds(78, 98, 200, 35);
-		add(txtLogin);
+		panel.add(txtLogin);
 		txtLogin.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(78, 187, 200, 35);
+		panel.add(txtPassword);
 		
 		JLabel lblLogin = new JLabel("Pseudonyme");
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLogin.setBounds(78, 69, 200, 19);
-		add(lblLogin);
+		panel.add(lblLogin);
 		
 		JLabel lblPassword = new JLabel("Mot de passe");
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblPassword.setBounds(78, 158, 200, 19);
-		add(lblPassword);
+		panel.add(lblPassword);
 		
 		JButton btnRegister = new JButton("S'inscrire");
 		btnRegister.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
-				myController.getMyView().getPnlConnexion().setVisible(false);
-				myController.getMyView().remove(myController.getMyView().getPnlConnexion());
-				myController.getMyView().setPnlConnexion(null);
-				
-				myController.getMyView().setPnlInscritpion(new PnlRegister(controller));
-				myController.getMyView().add(myController.getMyView().getPnlInscritpion());
-				
+				if(txtLogin.getText() == "") {
+					lblMessage.setText("Merci d'inscrire un pseudonyme");
+				} else if(txtPassword.getText() == "") {
+					lblMessage.setText("Merci d'inscrire un mot de passe");
+				} else {
+					try {
+						boolean runned = myController.getMyUser().createUser(txtLogin.getText(), txtPassword.getText());
+						//System.out.println("Runned :" + runned);
+						if (!runned) {
+							lblMessage.setText("Ce pseudonyme n'est pas disponible...");
+						}else {
+							myController.getMyView().getPnlConnexion().setVisible(false);
+							myController.getMyView().remove(myController.getMyView().getPnlConnexion());
+							myController.getMyView().setPnlConnexion(null);
+							
+							myController.getMyView().setPnlInscritpion(new PnlRegister(controller));
+							myController.getMyView().add(myController.getMyView().getPnlInscritpion());
+							
+						}
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
 		btnRegister.setBackground(new Color(255, 255, 255));
 		btnRegister.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnRegister.setBounds(10, 249, 140, 35);
-		add(btnRegister);
+		panel.add(btnRegister);
 		
 		JButton btnLogin = new JButton("S'indentifier");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-	
-	
+				try {
+					boolean runned = myController.getMyUser().connectUser(txtLogin.getText(), txtPassword.getText());
+					if (!runned) {
+						lblMessage.setText("Pseudonyme ou Mot de passe incorrect...");
+					}
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 					
 					myController.getMyView().getPnlConnexion().setVisible(false);
 					myController.getMyView().remove(myController.getMyView().getPnlConnexion());
@@ -87,30 +121,19 @@ public class PnlLogin extends JPanel {
 					
 					myController.getMyView().setPnlModejeu(new PnlGamemode(controller));
 					myController.getMyView().add(myController.getMyView().getPnlModejeu());
-					
-//				else {
-//					JLabel lblLoginerreur = new JLabel("Utilisateur ou mot de passe invalide");
-//					lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
-//					lblLogin.setHorizontalAlignment(SwingConstants.CENTER);
-//				}
+				}
 			}
 		});
 		btnLogin.setBackground(new Color(255, 255, 255));
 		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		btnLogin.setBounds(208, 249, 140, 35);
-		add(btnLogin);
+		panel.add(btnLogin);
 		
-		JLabel lblMessage = new JLabel("");
+		lblMessage = new JLabel("");
 		lblMessage.setForeground(new Color(255, 0, 0));
 		lblMessage.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMessage.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblMessage.setBounds(78, 230, 200, 19);
-		add(lblMessage);
-		
-		txtPassword = new JPasswordField();
-		txtPassword.setBounds(78, 187, 200, 35);
-		add(txtPassword);
-		
+		lblMessage.setBounds(10, 226, 338, 19);
+		panel.add(lblMessage);
 	}
-
 }
